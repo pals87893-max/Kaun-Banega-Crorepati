5e6708857f5aae326c1364dc7701285dff639c20
pals87893-max <pals87893@gmail.com>
2026-03-18 18:59:41 +0530

    Update Flask app configuration settings
    Added SQLALCHEMY_TRACK_MODIFICATIONS configuration and set debug mode to False.

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db = SQLAlchemy(app)
 
 class User(db.Model):  # Changed 'user' to 'db'
@@ -54,4 +55,4 @@ def game():
 if __name__=="__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=False)
