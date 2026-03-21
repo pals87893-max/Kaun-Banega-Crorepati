43108c9b74dad928c7d371e8a61da648a7fa7566
pals87893-max <pals87893@gmail.com>
2026-03-21 22:07:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask,render_template,redirect,url_for,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
